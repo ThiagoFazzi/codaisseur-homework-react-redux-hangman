4f0e1723c534647f8dcfab913ad0393516d23f09
ThiagoFazzi <thiago@xtwo.com.br>
2018-08-25 09:30:44 +0200

    imported the Provider component and setted the store on it in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux'
+import store from './store'
 import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -14,6 +18,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+      </Provider>
     );
   }
 }
