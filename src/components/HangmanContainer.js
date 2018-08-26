289b6ceb992a7f54dc952412a7bee895a0655353
ThiagoFazzi <thiago@xtwo.com.br>
2018-08-26 08:32:36 +0200

    commit game works

--- src/components/HangmanContainer.js
+++ src/components/HangmanContainer.js
@@ -11,45 +11,42 @@ class HangmanContainer extends React.PureComponent {
     componentDidMount(){
       this.props.newGame(randomWord())
     }
-
-    checkIfWin = () => {
-      const res = gameFinished(this.props.word, this.props.letters)
-      return res
-    }
-
-    
-    
     
     render(){
       if (!this.props.word) return 'Loading...'
-      if(gameFinished(this.props.word, this.props.letters)) return(
-      <div>
-        <HangmanWord               
-        word={showGuess(this.props.word, this.props.letters)}
-        letter={wrongGuessCount(this.props.word, this.props.letters)}
-        gameFinished={'win'}
-        //gameFinished={console.log(gameFinished(this.props.word, this.props.letters))}
-        //isWin={console.log(isWinner(this.props.word, this.props.letters))}
-        //isLoose={wrongGuessLimit(this.props.word, this.props.letters)}>
->
-      </HangmanWord>
-      <HangmanGuess letter={this.props.letters} makeGuess={this.props.makeGuess}></HangmanGuess>
-    </div>        
+      if(gameFinished(this.props.word, this.props.letters)) {
+        if(wrongGuessLimit(this.props.word, this.props.letters)) {
+          return(
+            <div>
+              <HangmanWord               
+                word={showGuess(this.props.word, this.props.letters)}
+                letter={wrongGuessCount(this.props.word, this.props.letters)}
+                gameFinished={'YOU LOOSE'}>
+              </HangmanWord>
+            </div>        
+          )
+        }
+        if(isWinner(this.props.word, this.props.letters)){
+          return(
+            <div>
+              <HangmanWord               
+                word={showGuess(this.props.word, this.props.letters)}
+                letter={wrongGuessCount(this.props.word, this.props.letters)}
+                gameFinished={'YOU WON'}>
+              </HangmanWord>
+            </div>        
+          )
+        }
+      }
+      return(
+        <div>      
+          <HangmanWord               
+            word={showGuess(this.props.word, this.props.letters)}
+            letter={wrongGuessCount(this.props.word, this.props.letters)}>
+          </HangmanWord>
+          <HangmanGuess letter={this.props.letters} makeGuess={this.props.makeGuess}></HangmanGuess>
+        </div>
       )
-        return(
-          <div>      
-            <HangmanWord               
-              word={showGuess(this.props.word, this.props.letters)}
-              letter={wrongGuessCount(this.props.word, this.props.letters)}
-              gameFinished={this.props.checkIfWin}
-              //gameFinished={console.log(gameFinished(this.props.word, this.props.letters))}
-              //isWin={console.log(isWinner(this.props.word, this.props.letters))}
-              //isLoose={wrongGuessLimit(this.props.word, this.props.letters)}>
->
-            </HangmanWord>
-            <HangmanGuess letter={this.props.letters} makeGuess={this.props.makeGuess}></HangmanGuess>
-          </div>
-        )
     }
 }
 
