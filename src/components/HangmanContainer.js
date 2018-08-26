2c04d31df9e7baf2e55ba8cd9f1acedefe3ada00
ThiagoFazzi <thiago@xtwo.com.br>
2018-08-26 08:20:02 +0200

    function to check if player won working

--- src/components/HangmanContainer.js
+++ src/components/HangmanContainer.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import HangmanWord from './HangmanWord'
 import HangmanGuess from './HangmanGuess'
-import { randomWord, showGuess } from '../lib/game'
+import { randomWord, showGuess, wrongGuessCount, gameFinished, isWinner, wrongGuessLimit } from '../lib/game'
 import { connect } from 'react-redux'
 import { newGame, makeGuess } from '../actions/game'
 
@@ -12,12 +12,40 @@ class HangmanContainer extends React.PureComponent {
       this.props.newGame(randomWord())
     }
 
+    checkIfWin = () => {
+      const res = gameFinished(this.props.word, this.props.letters)
+      return res
+    }
+
+    
+    
+    
     render(){
       if (!this.props.word) return 'Loading...'
+      if(gameFinished(this.props.word, this.props.letters)) return(
+      <div>
+        <HangmanWord               
+        word={showGuess(this.props.word, this.props.letters)}
+        letter={wrongGuessCount(this.props.word, this.props.letters)}
+        gameFinished={'win'}
+        //gameFinished={console.log(gameFinished(this.props.word, this.props.letters))}
+        //isWin={console.log(isWinner(this.props.word, this.props.letters))}
+        //isLoose={wrongGuessLimit(this.props.word, this.props.letters)}>
+>
+      </HangmanWord>
+      <HangmanGuess letter={this.props.letters} makeGuess={this.props.makeGuess}></HangmanGuess>
+    </div>        
+      )
         return(
           <div>      
             <HangmanWord               
-              word={showGuess(this.props.word, this.props.letters)}>
+              word={showGuess(this.props.word, this.props.letters)}
+              letter={wrongGuessCount(this.props.word, this.props.letters)}
+              gameFinished={this.props.checkIfWin}
+              //gameFinished={console.log(gameFinished(this.props.word, this.props.letters))}
+              //isWin={console.log(isWinner(this.props.word, this.props.letters))}
+              //isLoose={wrongGuessLimit(this.props.word, this.props.letters)}>
+>
             </HangmanWord>
             <HangmanGuess letter={this.props.letters} makeGuess={this.props.makeGuess}></HangmanGuess>
           </div>
--- src/components/HangmanWord.js
+++ src/components/HangmanWord.js
@@ -8,6 +8,9 @@ export default class HangmanWord extends React.PureComponent {
         <h1>NEW GAME - LETS START</h1>
         <p>{this.props.word}</p>
         <h2>{this.props.letter}</h2>
+        <h2>{this.props.gameFinished}</h2>
+        <h2>{this.props.isWin}</h2>
+        <h2>{this.props.isLoose}</h2>
       </div>
     )
   }
